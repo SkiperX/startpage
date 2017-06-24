9e70cd953bfd58a2042751c5a34c74a5f0361d1a
a.glebov <sanya2121@gmail.com>
2017-06-24 05:23:26 +0500

    3

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -26,23 +26,26 @@ $(function () {
                 data: $(this).serialize(),
                 success: function (data)
                 {
-                    console.log(data);
-                    // data = JSON object that contact.php returns
+                    if(data == "ok") {
+                        console.log(data);
+                        // data = JSON object that contact.php returns
 
-                    // we recieve the type of the message: success x danger and apply it to the 
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
+                        // we recieve the type of the message: success x danger and apply it to the
+                        var messageAlert = 'alert-success';
+                        var messageText = "Спасибо! Я свяжусь с Вами в ближайшее время";
 
-                    // let's compose Bootstrap alert box HTML
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    
-                    // If we have messageAlert and messageText
-                    if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
-                        $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
-                        $('#contact-form')[0].reset();
+                        // let's compose Bootstrap alert box HTML
+                        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+
+                        // If we have messageAlert and messageText
+                        if (messageAlert && messageText) {
+                            // inject the alert to .messages div in our form
+                            $('#contact-form').find('.messages').html(alertBox);
+                            // empty the form
+                            $('#contact-form')[0].reset();
+                        }
                     }
+
                 }
             });
             return false;
